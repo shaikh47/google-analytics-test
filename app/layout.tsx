9189f051da4cb25a37f8003c030b0d69faebe0cf
omar shaikh <omar.shaikh@bjitgroup.com>
2024-12-12 10:43:25 +0600

    added google manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="G-08G8W9R246" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
